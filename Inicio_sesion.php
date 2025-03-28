3c4ca5dd323d46ee5edd5a908f48622925fbbe5e
Diego Diaz <diegoa.diazr@konradlorenz.edu.co>
2025-03-28 13:32:38 -0500

    Formulario de inicio de sesión

---
+++ Inicio_sesion.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200;300;400&family=Orbitron:wght@300;400&family=Oxanium:wght@200;300;400&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <form action="PaginaPrincipal.php">
+        <!-- Div del header -->
+        <div class="header">
+            <div class="header-content">
+                <img src="https://pixelforgestudio.com/wp-content/uploads/2024/11/Pixel-Forge-Studio-Header-Logo.png" alt="LogoPixelForge">
+                <h2>INICIO DE SESIÓN</h2>
+            </div>
+        </div>
+
+        <br>
+        <!-- Div del Formulario -->
+        <div class="form-content">
+            <!-- Div del correo -->
+            <div class="input-container">
+                <i class="fa-solid fa-user"></i>
+                <input type="email" name="Correo" placeholder="Correo de usuario">
+            </div>
+
+            <!-- Div de la contraseña -->
+            <div class="input-container">
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" name="Contraseña" placeholder="Contraseña de usuario">
+            </div>
+
+            <button type="submit">Iniciar sesión</button>
+            <br><br>
+            <a href="CambiarContraseña.php">¿Desea cambiar u olvidó su contraseña?</a>
+        </div>
+    </form>
+
+</body>
+
+</html>
